72fbdf58c77fbf936364f6088bc209395d951de3
dmitriev_sergey <info@davra.cz>
2019-10-16 22:50:23 +0200

    FEATURE;Create welcome screen component

---
+++ src/components/app/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {WelcomeScreen} from '../welcome-screen/welcome-screen.jsx';
+
+export const App = (props) => {
+  const {gameTime, errorCount} = props;
+  return <WelcomeScreen gameTime={gameTime} errorCount={errorCount} />;
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number,
+  errorCount: PropTypes.number,
+};
